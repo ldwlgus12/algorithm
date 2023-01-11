48aa1316c48f4d1653862a33a41fb58f1201b3cd
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2023-01-11 15:38:47 +0900

    [D1] Title: 1대1 가위바위보, Time: 146 ms, Memory: 58,256 KB -BaekjoonHub

---
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -0,0 +1,14 @@
+A, B = map(int, input().split())
+
+if A == 2 and B == 1:
+    print("A")
+elif A == 3 and B == 2:
+    print("A")
+elif A == 1 and B == 3:
+    print("A")
+elif B == 2 and A == 1:
+    print("B")
+elif B == 3 and A == 2:
+    print("B")
+elif B == 1 and A == 3:
+    print("B")
