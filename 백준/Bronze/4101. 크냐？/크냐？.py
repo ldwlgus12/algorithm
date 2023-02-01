f27f8e877ac000e6bf048f4f8c1b1c12bbc3f3d6
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2023-02-01 21:05:56 +0900

    [Bronze V] Title: 크냐?, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        a, b = map(int, input().split())
+        if a == 0 and b == 0:
+            break
+        else:
+            if a > b:
+                print("Yes")
+            else:
+                print("No")
+        
+    except:
+        break
