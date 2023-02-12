8bed353ab17f611e442fe1b9d489f437d6fc06c4
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2023-02-12 21:56:30 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 244 ms, Memory: 41316 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -2,15 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-nums = list(map(int, input().split()))
-pre = [0]
-mysum = 0
+ns = list(map(int, input().split()))
+nlist = [0]
+hap = 0
 
 for i in range(n):
-  mysum += nums[i]
-  pre.append(mysum)
-
-
-for i in range(m):
-  a, b = map(int, input().split())
-  print(pre[b] - pre[a-1])
+    hap += ns[i]
+    nlist.append(hap)
+for j in range(m):
+    a, b = map(int, input().split())
+    print(nlist[b] - nlist[a-1])
