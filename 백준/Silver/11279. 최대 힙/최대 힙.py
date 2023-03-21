4b84615cfa134bda6a33e8d9e130bb877f652e6a
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2023-03-21 22:49:17 +0900

    [Silver II] Title: 최대 힙, Time: 156 ms, Memory: 46396 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,19 @@
+from heapq import *
+import sys
+input = sys.stdin.readline
+
+heap = []
+n = int(input())
+
+for i in range(n):
+    m = int(input())
+
+    if m == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heappop(heap)[1])
+    else:
+        heappush(heap, (-m, m))
+
+
