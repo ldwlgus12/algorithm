f9d9ff3d639480d9b4fdbd726fc6ce5106abcb49
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2023-03-26 04:14:01 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -0,0 +1,13 @@
+def solution(angle):
+    answer = 0
+    
+    if angle > 0 and angle < 90:
+        answer = 1
+    elif angle == 90:
+        answer = 2
+    elif angle > 90 and angle < 180:
+        answer = 3
+    else:
+        answer = 4
+    
+    return answer
