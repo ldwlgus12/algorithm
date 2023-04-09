b580088d671aa25b5e39eddff85b8cbe11c4901f
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2023-04-09 16:03:59 +0900

    [level 2] Title: 구명보트, Time: 8.50 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -3,10 +3,10 @@ def solution(people, limit) :
     people.sort()
 
     a = 0
-    b = len(people) - 1
-    while a < b:
-        if people[b] + people[a] <= limit:
+    b = len(people) - 1     # indexing으로 비교하기 위해 배열의 길이에서 1을 빼줌
+    while a <= b:
+        answer += 1
+        if people[b] + people[a] <= limit:  # 가장 몸무게가 큰 사람과 작은 사람을 더해 limit 값보다 작거나 같으면 태우고, 크면 가장 큰 사람만 태움
             a += 1
-            answer += 1
         b -= 1
-    return len(people) - answer
+    return answer
