05fdc130f6e4ae90be9f0c4685b7cf49cbf5e008
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2023-06-19 20:59:28 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+a = input()
+
+if a == 'SONGDO':
+    print('HIGHSCHOOL')
+elif a == 'CODE':
+    print('MASTER')
+elif a == '2023':
+    print('0611')
+elif a == 'ALGORITHM':
+    print('CONTEST')
