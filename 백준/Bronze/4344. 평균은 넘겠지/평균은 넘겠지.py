1129f8aea263642f5d366f383726790b6e142403
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2023-06-28 11:23:03 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,17 +1,11 @@
-import sys
-C = int(input())
+a = int(input())
 
-for i in range(C):
-    ave = 0
-    student = 0
-    percent = 0
-    score = list(map(int, sys.stdin.readline().split()))
-    score_middle = score[1:]
-    ave = (sum(score_middle)/score[0])
-
-    for j in range(len(score_middle)):
-        if score_middle[j] > ave:
-            student += 1
-    
-    percent = (100*(student/score[0]))
-    print(f'{percent:.3f}%')
+for i in range(a):
+    num = list(map(int, input().split()))
+    avg = sum(num[1:])/num[0]
+    cnt = 0
+    for i in range(1, len(num)):
+        if num[i] > avg:
+            cnt += 1
+            
+    print('%.3f' % (cnt / num[0] * 100) + '%')
