4415c5873f4089eeec54d2889ba57b9d2d4a7225
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2023-08-02 18:23:07 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+def dp(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return dp(n-1) + dp(n-2) + dp(n-3)
+    
+for i in range(t):
+    n = int(input())
+    print(dp(n))
+    
