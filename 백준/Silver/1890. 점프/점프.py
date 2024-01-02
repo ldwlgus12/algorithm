68a35b51407a65079121c013ba859a2d4effe2da
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2024-01-03 01:04:25 +0900

    [Silver I] Title: 점프, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,20 @@
+import sys
+n = int(sys.stdin.readline())
+
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+dp = [[0]*n for _ in range(n)]
+dp[0][0] = 1
+
+for i in range(n):
+    for j in range(n):
+        
+        if i == n-1 and j == n-1:
+            print(dp[i][j])
+            break
+
+        if j + graph[i][j] < n:
+            dp[i][j + graph[i][j]] += dp[i][j]
+
+        if i + graph[i][j] < n:
+            dp[i + graph[i][j]][j] += dp[i][j]
+            
