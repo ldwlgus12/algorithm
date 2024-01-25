3bcc96361d298131d5435bf03a4ffa8e6749431c
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2024-01-25 16:18:17 +0900

    [Silver V] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,19 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+result = 0
+
+while True:
+    if n % 5 == 0:
+        result += n//5
+        break
+    else:
+        n -= 2
+        result += 1
+
+    if n < 0:
+        break
+
+if n < 0:
+    print(-1)
+else:
+    print(result)
