1929bffbe4e5691a2385860288ad5537edea2cdf
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2024-02-27 19:09:38 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+n, m = map(int, sys.stdin.readline().split())
+graph = []
+
+dx = [-1, 1, 0, 0] 
+dy = [0, 0, -1, 1]
+
+for i in range(n):
+    graph.append(list(map(int, sys.stdin.readline().rstrip()))) 
+
+def bfs(x, y):   
+    queue = deque()
+    queue.append((x,y))
+    
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1    
+    return graph[n-1][m-1]
+
+print(bfs(0,0))
