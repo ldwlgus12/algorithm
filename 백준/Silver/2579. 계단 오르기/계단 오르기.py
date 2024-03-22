5b5361ba6f9efc0fef0b10ddd668107ff09cb9fa
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2024-03-23 00:44:48 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,15 @@
+n = int(input())
+stair = [0] * 301
+dp = [0] * 301
+
+for i in range(n):
+    stair[i] = int(input())
+
+dp[0] = stair[0]
+dp[1] = stair[0] + stair[1]
+dp[2] = max(stair[0] + stair[2], stair[1] + stair[2])
+
+for i in range(3, n):
+    dp[i] = max(dp[i-3] + stair[i-1] + stair[i], dp[i-2] + stair[i])
+
+print(dp[n-1])
