a8f3a2548f7aa496939d7a01f15479d991125bdf
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2024-07-20 00:13:51 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,13 @@
+name = input()
+
+if name == "M":
+    print("MatKor")
+elif name == "W":
+    print("WiCys")
+elif name == "C":
+    print("CyKor")
+elif name == "A":
+    print("AlKor")
+else:
+    print("$clear")
+    
