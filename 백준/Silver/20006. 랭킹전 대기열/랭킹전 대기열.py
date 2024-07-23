9de3303f859552e3371fee8be47e4973be13440a
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2024-07-24 02:55:40 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -0,0 +1,31 @@
+p, m = map(int, input().split())
+room = []
+
+for i in range(p):
+    l, n = input().split()
+    if not room:
+        room.append([[int(l), n]])
+        continue
+	
+    enter = False
+    for i in room:
+        if len(i) < m and i[0][0]-10 <= int(l) <= i[0][0]+10:
+            i.append([int(l), n])
+            enter = True
+            break
+            
+    if not enter:
+        room.append([[int(l), n]])
+   
+for i in room:
+    i.sort(key=lambda x:x[1])
+
+for i in room:
+    if len(i) == m:
+        print('Started!')
+    else:
+        print('Waiting!')
+        
+    for l, n in i:
+        print(l, n)
+        
