ee684fbcdd04508c5b7d430eddfe46683fb4b7f1
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2024-08-05 16:36:04 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * factorial(n-1)
+
+print(factorial(n) // (factorial(n-k) * factorial(k)))
