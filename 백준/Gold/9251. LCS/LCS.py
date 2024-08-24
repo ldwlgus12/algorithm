f04e940587dc540ae8a1f5b43755f467ee4bf2b9
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2024-08-24 22:13:02 +0900

    [Gold V] Title: LCS, Time: 492 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,12 @@
+str1 = list(input())
+str2 = list(input())
+lcs = [[0] * (len(str2)+1) for _ in range(len(str1)+1)]
+
+for i in range(1, len(str1)+1):
+    for j in range(1, len(str2)+1):
+        if str1[i-1] == str2[j-1]:
+            lcs[i][j] = lcs[i-1][j-1] + 1
+        else:
+            lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])   
+            
+print(max(map(max, lcs)))
