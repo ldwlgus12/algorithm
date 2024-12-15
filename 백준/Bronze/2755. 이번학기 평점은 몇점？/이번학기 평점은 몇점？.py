6b28e35722765299e4fcae016c142b412a6e1646
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2024-12-16 03:30:59 +0900

    [Bronze I] Title: 이번학기 평점은 몇점?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+grade = {'A+': 4.3, 'A0': 4.0, 'A-': 3.7,
+        'B+': 3.3, 'B0': 3.0, 'B-': 2.7,
+        'C+': 2.3, 'C0': 2.0, 'C-': 1.7,
+        'D+': 1.3, 'D0': 1.0, 'D-': 0.7, 'F': 0.0}
+
+n = int(input())
+result = 0
+total = 0
+
+for i in range(n):
+    lst = list(input().split())
+    total += int(lst[1])    
+    result += int(lst[1]) * grade[lst[2]]
+    
+print(f'{result/total + 10**-10:.2f}')
