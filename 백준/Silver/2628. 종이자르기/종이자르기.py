b9b194010a04c1fb37a96e021a64ad36aad6d074
전지현 (Jihyeon Jeon) <121420134+ldwlgus12@users.noreply.github.com>
2025-02-06 23:09:39 +0900

    [Silver V] Title: 종이자르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -0,0 +1,24 @@
+w, h = map(int, input().split())
+n = int(input())
+
+width = [0, w]
+height = [0, h]
+result = 0
+ 
+for i in range(n):
+    a, b = map(int, input().split())
+    if a == 0:
+        height.append(b)
+    elif a == 1:
+        width.append(b)
+
+width.sort()
+height.sort() 
+ 
+for i in range(len(width)-1):
+    for j in range(len(height)-1):
+        x = width[i+1] - width[i]
+        y = height[j+1] - height[j]
+        result = max(result, x*y)
+
+print(result)
